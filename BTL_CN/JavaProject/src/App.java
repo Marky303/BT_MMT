e1825098309e1cdb3c197caa1e5df0ce4f52bddf
NhienHuu <102888254+Marky303@users.noreply.github.com>
2023-11-02 20:22:24 +0700

    Add files via upload

---
+++ BTL_CN/JavaProject/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("Application Started");
+        CommandController cmd = new CommandController();
+        cmd.inputController();
+    }
+}
