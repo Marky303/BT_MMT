d156bdd028fd2978797cf863d37fe9c4b8239f53
NhienHuu <102888254+Marky303@users.noreply.github.com>
2023-11-02 20:22:52 +0700

    Delete BTL_CN/JavaProject directory

--- BTL_CN/JavaProject/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception 
-    {
-        System.out.println("Application Started");
-        CommandController cmd = new CommandController();
-        cmd.inputController();
-    }
-}
