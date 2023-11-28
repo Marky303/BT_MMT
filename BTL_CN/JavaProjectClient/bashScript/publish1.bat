95e2fc4223fb2dbc7269925dc831fbeca4fdc404
NhienHuu <102888254+Marky303@users.noreply.github.com>
2023-11-28 17:54:15 +0700

    Delete BTL_CN directory

--- BTL_CN/JavaProjectClient/bashScript/publish1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-    echo publish abc| ncat localhost 1232
-ncat -z --recv-only localhost 1232
--- BTL_CN/JavaProjectServer/bashScript/discover.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-    echo discover| ncat localhost 1233
-ncat -z --recv-only localhost 1233
